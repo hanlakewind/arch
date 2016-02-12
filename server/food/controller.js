db04174426ac2f4a1851ad667db15ec19f837847
Han Li <abovemyheaven@gmail.com>
2016-02-11 21:44:16 -0700

    Updated food controller

--- server/food/controller.js
+++ server/food/controller.js
@@ -2,6 +2,7 @@
 
 var Food = require('../model/food');
 
-module.exports.createUser = function(request, response) {
-    
+module.exports.createFood = function(request, response) {
+    var foodName = request.body.foodName;
+    var foodCategoryId = request.body.foodCategoryId;
 };
