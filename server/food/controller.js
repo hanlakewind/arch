9723ff869c037757bcb032f57f0e9460822c2c54
Han Li <abovemyheaven@gmail.com>
2016-02-12 21:40:05 -0700

    Updated food controller

--- server/food/controller.js
+++ server/food/controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var mongoose = require('mongoose');
 var Food = require('../model/food');
+var ObjectId = mongoose.Types.ObjectId;
 
 module.exports.createFood = function(request, response) {
-    var foodName = request.body.foodName;
     var foodCategoryId = request.body.foodCategoryId;
+    FoodCategory.findOne({
+        _id: ObjectId(foodCategoryId)
+    }, function(error, foundFoodCategory) {
+        if(error) {
+            return request.status(500).send(error);
+        } else {
+
+        }
+    });
 };
